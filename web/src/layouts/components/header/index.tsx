79449e88da65cf4dbc19d320199a19fc7242d922
DoctorAndy <andyh0510@qq.com>
2026-01-09 14:15:47 +0800

    fix: 恢复移动端头像在右边
    - 撤销 row-reverse，头像保持在右边

--- web/src/layouts/components/header/index.tsx
+++ web/src/layouts/components/header/index.tsx
@@ -52,7 +52,6 @@ const RagHeader = () => {
         justifyContent: 'space-between',
         alignItems: 'center',
         height: isMobile ? '56px' : '72px',
-        flexDirection: isMobile ? 'row-reverse' : 'row',
       }}
     >
       <a>
